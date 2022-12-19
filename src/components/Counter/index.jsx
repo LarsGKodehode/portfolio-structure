92a8dc0cb15bdcc60a2e6985912d2ab050c14cea
LarsG <lars.ga.solheim@gmail.com>
2022-12-19 13:02:55 +0100

    added Counter component

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,26 @@
+// Libraries
+import { useState } from 'react';
+
+// CSS
+import styles from './layout.module.css'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(
+      (oldCount) => oldCount + 1
+    )
+  }
+
+  return (
+    <div>
+      <h4>Counter</h4>
+      <h5>{`Count: ${count}`}</h5>
+
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  )
+}
+
+export default Counter;
