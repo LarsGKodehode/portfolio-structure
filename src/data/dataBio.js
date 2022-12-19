747d566d03002938b4b37d9dd8b0852228e32262
LarsG <lars.ga.solheim@gmail.com>
2022-12-19 13:35:56 +0100

    added data file

---
+++ src/data/dataBio.js
@@ -0,0 +1,12 @@
+const dataBio = {
+  nameFirst: 'Lars',
+  nameMiddle: [ 'Gunnar' ],
+  interests: [
+    'tech',
+    'nature',
+    'mathematics'
+  ],
+  short: 'Enjoys figuring out how stuff works'
+}
+
+export default dataBio;
