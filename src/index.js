2e53fea0c1296646834e4c053a61cb986af8e143
LarsG <lars.ga.solheim@gmail.com>
2022-12-19 13:08:59 +0100

    added comment regarding CSS import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// CSS
+// CSS Globals
 import './styles/basic.css'
 import './styles/palette.css'
 import './styles/spacing.css'
