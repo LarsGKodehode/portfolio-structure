6eec7e1e854ed0d4b6395522d5716673e9b0a4cf
LarsG <lars.ga.solheim@gmail.com>
2022-12-19 13:02:35 +0100

    added LandingPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+// Libraries
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+// Routes
+import LandingPage from './routes';
+import ContactPage from './routes/Contact';
+
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+
+        <Route path='/' element={<LandingPage />}>
+          <Route path='/Contact' element={<ContactPage />} />
+        </Route>
+
+        <Route path='*' element={<h1>Error 404</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/routes/index.jsx
@@ -0,0 +1,34 @@
+import {
+  Outlet,
+  Link
+} from 'react-router-dom'
+
+// CSS
+import styles from './layout.module.css'
+
+function LandingPage() {
+  return (
+    <>
+    {/* header */}
+    <header>
+      <h1>Landing Page</h1>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/Contact'>Contact</Link>
+      </nav>
+    </header>
+
+    {/* Here we will put all our "subpages" */}
+    <main className={styles.grow}>
+      <Outlet />
+    </main>
+
+    {/* footer */}
+    <footer>
+      <a href="www.github.com">GitHub</a>
+    </footer>
+    </>
+  )
+}
+
+export default LandingPage;
