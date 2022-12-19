c46a189c813c87e3e8936b30eb6d59b7d528def1
LarsG <lars.ga.solheim@gmail.com>
2022-12-19 13:02:45 +0100

    added Contact Page

---
+++ src/routes/Contact/index.jsx
@@ -0,0 +1,15 @@
+import Counter from '../../components/Counter';
+
+// CSS
+import styles from './layout.module.css'
+
+function ContactPage() {
+  return (
+    <>
+      <h1>Contact</h1>
+      <Counter />
+    </>
+  )
+}
+
+export default ContactPage;
